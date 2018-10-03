9de6ec2b7e1fca6e290d83a9775a270a7b56b950
javier <richards.javier@gmail.com>
2018-10-03 14:40:31 -0300

    se quitaron filtros de la tabla

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -20,9 +20,9 @@ class App extends Component {
 
   render() {
     return (
-      <SideBar> 
+      // <SideBar> 
         <Table datos={rows}/> 
-      </SideBar> 
+      // </SideBar> 
       
 
       
@@ -39,17 +39,3 @@ const loadingPromise = fetch('/express')
   .then(json => {
      rows = json
   })
-  // let id = 0;
-// function createData(familia, genero, especie, calidad, tamaño, pais, comentario, precio) {
-//   id += 1;
-//   return { id, familia, genero, especie, calidad, tamaño, pais, comentario, precio };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', "s", 6.0, 24, 4.0, 12, 12, 12, 12),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 12, 12, 12, 12),
-//   createData('Eclair', 262, 16.0, 24, 6.0, 12, 12, 12, 12),
-//   createData('Cupcake', 305, 3.7, 67, 4.3, 12, 12, 12, 12),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9, 12, 12, 12, 12),
-// ];
-
--- client/src/components/table.js
+++ client/src/components/table.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import MUIDataTable from "mui-datatables";
 
 
@@ -21,9 +14,86 @@ const styles = theme => ({
   },
 });
 
-const columns = ["Familia", "Genero", "Especie", "Calidad","Tamaño","País","Comentario","Precio","habitad"];
+// const columns = ["Familia", "Genero", "Especie", "Calidad","Tamaño","País","Comentario","Precio","habitad"];
+const columns = [
+  {
+   name: "Familia",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
+   
+  {
+   name: "Genero",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
 
+  {
+   name: "Especie",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
 
+  {
+   name: "Calidad",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
+
+  {
+   name: "Tamaño",
+   options: {
+    filter: false,
+    sort: false
+   }
+  },
+
+  {
+   name: "País",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
+
+  {
+   name: "Comentario",
+   options: {
+    filter: false,
+    sort: false
+   }
+  },
+
+  {
+   name: "Precio",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
+
+  {
+   name: "Habitat",
+   options: {
+    filter: true,
+    sort: false
+   }
+  },
+
+ ];
+
+const options = {
+  filterType: 'dropdawn',
+  responsive: 'stacked'
+};
 
 function SimpleTable(props) {
   var arr = props.datos.map(dato=>Object.values(dato))
@@ -32,9 +102,10 @@ function SimpleTable(props) {
  
     
     <MUIDataTable
-      title={"Employee List"}
+      title={"NutsForShells"}
       data={arr}
       columns={columns}
+      options={options}
     />
   );
 }
@@ -43,6 +114,6 @@ SimpleTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleTable);
+export default (SimpleTable);
 
 
