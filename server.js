a2766c595d1ecface00ac94d2f574c9330661fc1
javier <richards.javier@gmail.com>
2019-07-19 23:17:45 -0400

    creada la conexion hacia servidor ninjahosting

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var mysql      = require('mysql');
 const port = process.env.PORT || 5000;
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'shells'
+  host     : 'ftp.museonaturalia.cl',
+  user     : 'museonat_javier',
+  password : 'javier123.',
+  database : 'museonat_shells'
 
 });
 connection.connect(); 
